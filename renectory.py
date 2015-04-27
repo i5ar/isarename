8e6098caa4b778fc35f3c62c35f7354558b3f913
iSar <i5ar@live.it>
2015-04-26 20:55:05 -0700

    Update renectory.py

--- renectory.py
+++ renectory.py
@@ -5,4 +5,4 @@ import os
 class dirWalk(object):
     def __init__(self):
         '''Get directory path name'''
-
+        self.dir_name = str(input('Please, insert the directory path or press Enter for the current directory: ') or '.')
